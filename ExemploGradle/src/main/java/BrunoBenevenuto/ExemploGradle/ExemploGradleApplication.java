14a637b1a015201d359528eacdb1c6021f2797d5
Bruno Benevenuto <bruno.nascimenttoo@gmail.com>
2020-04-20 01:40:11 -0300

    Exemplo SpringBoot com Gradle

---
+++ ExemploGradle/src/main/java/BrunoBenevenuto/ExemploGradle/ExemploGradleApplication.java
@@ -0,0 +1,15 @@
+package BrunoBenevenuto.ExemploGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploGradleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ExemploGradleApplication.class, args);
+		System.out.println("Hello World!!!");
+	}
+
+}
