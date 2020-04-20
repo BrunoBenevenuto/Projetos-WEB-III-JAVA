0067726020543c49faee928a3c80445afad103ad
Bruno Benevenuto <bruno.nascimenttoo@gmail.com>
2020-04-20 12:30:24 -0300

    Exemplo de SpringBoot com Maven

---
+++ ExemploMaven/src/test/java/Bruno/Benevenuto/ExemploMaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package Bruno.Benevenuto.ExemploMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
